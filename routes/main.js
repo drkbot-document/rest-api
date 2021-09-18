e4858bed60c3d99077c33ab93621630c6e29860b
drkbot-document <90969374+drkbot-document@users.noreply.github.com>
2021-09-18 17:02:43 -0500

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -24,10 +24,10 @@ router.get('/config', (req, res) => {
         status: true,
         result: {
             prefix : '/',
-            namabot: 'ZhirrrBot',
-            namaowner: 'Zhirrr',
-            instagram: 'zhirr_ajalah',
-            youtube : 'Gak Punya'
+            namabot: 'DrkBot',
+            namaowner: 'ian-darkbox',
+            instagram: 'iand_tv',
+            youtube : '#'
         }
     }
     res.json(config)
